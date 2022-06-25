516a493c20f9cbf44a3bb37da6a3f5f7be1de2d0
yaalexa <yaalexa25@gmail>
2022-06-24 20:33:03 -0500

    segundo commit

--- ejemplo1.js
+++ ejemplo1.js
@@ -1,3 +1,15 @@
 class Libros{
-    
+    constructor(autor,nombre,numeropaginas){
+        this.autor=autor
+        this.nombre=nombre
+        this.numeropaginas=numeropaginas
+    }
+    set nombrelibro(nombre1){
+
+    }
+    get mostrarlibro(){
+
+    }
 }
+let libro1=new Libros("Gabriel garcia marquez","cien años de soledad", "350")
+console.log(libro1)
