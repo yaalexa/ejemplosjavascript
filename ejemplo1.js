73dfcab8c31a9f62171a70871f380d716c296f22
yaalexa <yaalexa25@gmail>
2022-06-24 21:43:16 -0500

    tercer comit

--- ejemplo1.js
+++ ejemplo1.js
@@ -1,14 +1,18 @@
+const _private=new WeakMap();
 class Libros{
     constructor(autor,nombre,numeropaginas){
-        this.autor=autor
-        this.nombre=nombre
-        this.numeropaginas=numeropaginas
+        const propiedades={
+            _autor:autor,
+            _nombre:nombre,
+            _numeropaginas:numeropaginas
+        }
+        _private.set(this,{propiedades})
     }
     set nombrelibro(nombre1){
-
+       return _private.get(this).propiedades['_nombre']=nombre1
     }
     get mostrarlibro(){
-
+        return _private.get(this).propiedades['_nombre'];
     }
 }
 let libro1=new Libros("Gabriel garcia marquez","cien años de soledad", "350")
